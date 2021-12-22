ea982ccdf79fa500cc8c0261d76812cdfd1a2c2f
Veronica122 <Veronicarodriguez.niu@gmail.com>
2021-12-22 08:55:34 -0600

    Entidad Persona

---
+++ DAL/Modelo/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Modelo
+{
+    public class Persona
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        [Required(ErrorMessage = "Sus nombres son requeridos")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "Sus apellidos son requeridos")]
+        public string Apellidos { get; set; }
+        [Required (ErrorMessage ="La edad es requerida") ]
+        [Range(1,130, ErrorMessage = "La edad debe ser valida")]
+        public int Edad { get; set; }
+        [Required (ErrorMessage ="Debe seleccionar una opcion")]
+        public bool Sexo { get; set; }
+        [Required (ErrorMessage ="El telefono es requerido")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "La direccion es requerida")]
+        public string Direccion { get; set; }
+    }
+}
