1990452d6b19b16f0ba6d02e95fcbc77415aa850
Veronica122 <Veronicarodriguez.niu@gmail.com>
2021-12-22 09:14:21 -0600

    >>>>Migracion<<<< enable-migrations add-migration update-database

---
+++ DAL/Migrations/202112221512489_inicial.cs
@@ -0,0 +1,31 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Personas",
+                c => new
+                    {
+                        IdPersona = c.Int(nullable: false, identity: true),
+                        Nombres = c.String(nullable: false),
+                        Apellidos = c.String(nullable: false),
+                        Edad = c.Int(nullable: false),
+                        Sexo = c.Boolean(nullable: false),
+                        Telefono = c.String(nullable: false),
+                        Direccion = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdPersona);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Personas");
+        }
+    }
+}
--- DAL/Modl/Contexto.cs
+++ DAL/Modl/Contexto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL.Modelo
+namespace DAL.Modl
 {
     public class Contexto : DbContext
     {
