ca88545eb26d2c3cf871e689a7e0c09c82837456
Veronica122 <Veronicarodriguez.niu@gmail.com>
2021-12-22 09:06:21 -0600

    Contexto -> DbSet<Persona>

---
+++ DAL/Modelo/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Modelo
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
